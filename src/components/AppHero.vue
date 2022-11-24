838d02d69ed72c6a0196d809b183696ea2dff163
Rafa <rafael.martinez@outlook.it>
2022-11-24 09:14:43 +0000

    Update AppHero.vue

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -12,11 +12,7 @@ export default {
   <h1>{{ hero }}</h1>
 </template>
 
-<style scoped>
-    template {
-        
-    }
-    
+<style scoped>    
     h1 {
         text-align: center;
         color: greenyellow;
